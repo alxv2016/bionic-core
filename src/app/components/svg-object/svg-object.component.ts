867db1b7213c4f5fba64440e9e7e8c518af94570
Alex Vong <avong2011@gmail.com>
2021-04-14 19:22:54 -0400

    fix: animation optimization

--- src/app/components/svg-object/svg-object.component.ts
+++ src/app/components/svg-object/svg-object.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   ElementRef,
   HostBinding,
+  NgZone,
   QueryList,
   Renderer2,
   ViewChild,
@@ -26,123 +27,126 @@ export class SvgObjectComponent implements AfterViewInit {
   @ViewChildren('svgPath3', {read: ElementRef}) svgPath3!: QueryList<ElementRef>;
   @ViewChildren('svgPath4', {read: ElementRef}) svgPath4!: QueryList<ElementRef>;
   @ViewChildren('grads', {read: ElementRef}) grads!: QueryList<ElementRef>;
-  constructor(private element: ElementRef, private render: Renderer2) {
+  constructor(private element: ElementRef, private render: Renderer2, private ngZone: NgZone) {
     gsap.registerPlugin(ScrollTrigger);
   }
 
   ngAfterViewInit(): void {
-    const svgPaths = this.svgPath.map((cir) => cir.nativeElement);
-    const svgPaths2 = this.svgPath2.map((cir) => cir.nativeElement);
-    const svgPaths3 = this.svgPath3.map((cir) => cir.nativeElement);
-    const svgPaths4 = this.svgPath4.map((cir) => cir.nativeElement);
-    const grads = this.grads.map((cir) => cir.nativeElement);
+    this.ngZone.runOutsideAngular(() => {
+      const svgPaths = this.svgPath.map((cir) => cir.nativeElement);
+      const svgPaths2 = this.svgPath2.map((cir) => cir.nativeElement);
+      const svgPaths3 = this.svgPath3.map((cir) => cir.nativeElement);
+      const svgPaths4 = this.svgPath4.map((cir) => cir.nativeElement);
+      const grads = this.grads.map((cir) => cir.nativeElement);
 
-    const backdrop = gsap.timeline({
-      defaults: {
-        transformOrigin: '50% 50%',
-        ease: 'back',
-        stagger: {
-          amount: 1.25,
-          from: 'end',
-        },
-        repeat: -1,
-        yoyo: true,
-        yoyoEase: true,
-        duration: 4,
-      },
-    });
-
-    backdrop.fromTo(
-      grads,
-      {
-        opacity: 0,
-        fill: 'cyan',
-      },
-      {
-        opacity: 1,
-        fill: '#DBFF01',
-        scaleX: 0.0125,
-        scaleY: 2,
-      }
-    );
-
-    gsap.fromTo(
-      this.reactorCore.nativeElement,
-      {
-        y: -4,
-        x: -4,
-      },
-      {
-        y: 4,
-        x: 4,
-        ease: 'power2.inOut',
-        repeat: -1,
-        duration: 1,
-        yoyo: true,
-      }
-    );
-
-    gsap.to(this.core.nativeElement, {
-      attr: {
-        offset: 0.11,
-      },
-      repeat: -1,
-      ease: 'back',
-      yoyo: true,
-    });
-
-    const staggering = gsap.timeline({
-      defaults: {
-        transformOrigin: '50% 50%',
-        ease: 'power2.inOut',
-        duration: 0.95,
-        repeat: -1,
-        stagger: 0.0125,
-        //yoyo: true,
-        // yoyoEase: true,
-      },
-    });
-
-    staggering
-      .to(svgPaths, {
-        attr: {
-          d: 'M350 241C350 300.923 301.423 349.5 241.5 349.5C181.577 349.5 133 300.923 133 241',
-        },
-      })
-      .to(
-        svgPaths4,
-        {
-          attr: {
-            d: 'M426 241C426 343.173 343.173 426 241 426C138.827 426 56 343.173 56 241',
+      const backdrop = gsap.timeline({
+        defaults: {
+          transformOrigin: '50% 50%',
+          ease: 'back',
+          force3D: true,
+          stagger: {
+            amount: 1.25,
+            from: 'end',
           },
-        },
-        0.175
-      )
-      .to(
-        svgPaths2,
-        {
-          attr: {
-            d: 'M350 240.5C350 180.577 301.423 132 241.5 132C181.577 132 133 180.577 133 240.5',
-          },
-        },
-        0
-      )
-      .to(
-        svgPaths3,
-        {
-          attr: {
-            d: 'M426 241C426 138.827 343.173 56 241 56C138.827 56 56 138.827 56 241',
-          },
-        },
-        0.175
-      )
-      .to(
-        this.circle.nativeElement,
-        {
-          rotate: 360,
+          repeat: -1,
+          yoyo: true,
+          yoyoEase: true,
           duration: 4,
         },
-        0.185
+      });
+
+      backdrop.fromTo(
+        grads,
+        {
+          opacity: 0,
+          fill: 'cyan',
+        },
+        {
+          opacity: 1,
+          fill: '#DBFF01',
+          scaleX: 0.0125,
+          scaleY: 2,
+        }
       );
+
+      gsap.fromTo(
+        this.reactorCore.nativeElement,
+        {
+          y: -4,
+          x: -4,
+        },
+        {
+          y: 4,
+          x: 4,
+          ease: 'power2.inOut',
+          repeat: -1,
+          duration: 1,
+          yoyo: true,
+        }
+      );
+
+      gsap.to(this.core.nativeElement, {
+        attr: {
+          offset: 0.11,
+        },
+        repeat: -1,
+        ease: 'back',
+        yoyo: true,
+      });
+
+      const staggering = gsap.timeline({
+        defaults: {
+          transformOrigin: '50% 50%',
+          ease: 'power2.inOut',
+          duration: 0.95,
+          repeat: -1,
+          stagger: 0.0125,
+          //yoyo: true,
+          // yoyoEase: true,
+        },
+      });
+
+      staggering
+        .to(svgPaths, {
+          attr: {
+            d: 'M350 241C350 300.923 301.423 349.5 241.5 349.5C181.577 349.5 133 300.923 133 241',
+          },
+        })
+        .to(
+          svgPaths4,
+          {
+            attr: {
+              d: 'M426 241C426 343.173 343.173 426 241 426C138.827 426 56 343.173 56 241',
+            },
+          },
+          0.175
+        )
+        .to(
+          svgPaths2,
+          {
+            attr: {
+              d: 'M350 240.5C350 180.577 301.423 132 241.5 132C181.577 132 133 180.577 133 240.5',
+            },
+          },
+          0
+        )
+        .to(
+          svgPaths3,
+          {
+            attr: {
+              d: 'M426 241C426 138.827 343.173 56 241 56C138.827 56 56 138.827 56 241',
+            },
+          },
+          0.175
+        )
+        .to(
+          this.circle.nativeElement,
+          {
+            rotate: 360,
+            duration: 4,
+          },
+          0.185
+        );
+    });
   }
 }
